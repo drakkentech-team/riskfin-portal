5bccccc69c102e253015985133c7d6704512e16c
Alexan-Anderson <alexanranderson@gmail.com>
2023-09-26 10:54:07 +0200

    Overhaul

--- src/utils/config.js
+++
@@ -1,8 +0,0 @@
-const config = 
-  {
-    local: "http://localhost:9000",
-    local2: "http://localhost:5000",
-    bearerToken: "1HW94aH3Gu9BNxqw2QnY4y7zMa1xwlm_rg2ZiA9tt3fu"
-  }
-
-export default config
